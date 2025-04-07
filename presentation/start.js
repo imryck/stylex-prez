ce34d81c4fc7112b945051b25f242fcd9a0dbc41
David MAURIN <d.maurin@groupeonepoint.com>
2025-04-07 15:09:55 +0200

    maj charte devoxx

--- presentation/start.js
+++ presentation/start.js
@@ -22,8 +22,10 @@ const convertAsciidoc = () => {
   });
   fs.copyFileSync('css/custom.css', 'dist/custom.css');
   fs.copyFileSync('./node_modules/highlightjs/styles/atom-one-dark.css', 'dist/atom-one-dark.css');
+  fs.copyFileSync('./node_modules/highlightjs/styles/a11y-light.css', 'dist/a11y-light.css');
   fs.cpSync('./slides/images', 'dist/images', {recursive: true});
   fs.cpSync('./slides/videos', 'dist/videos', {recursive: true});
+  fs.cpSync('./slides/fonts', 'dist/fonts', {recursive: true});
 }
 fs.rmSync(path.resolve(__dirname, 'dist'), { recursive: true, force: true })
 convertAsciidoc();
